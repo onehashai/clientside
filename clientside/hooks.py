1ff5a98ddba6f768ce3684a262ba1b0cfa82582f
rohit.kumar-at-942428920331 <59276987+RohitKumarGit@users.noreply.github.com>
2023-06-08 00:59:51 +0530

    eqre

--- clientside/hooks.py
+++ clientside/hooks.py
@@ -42,7 +42,7 @@ app_license = "MIT"
 
 # website user home page (by Role)
 # role_home_page = {
-#	"Role": "home_page"
+# 	"Role": "home_page"
 # }
 
 # Generators
@@ -56,15 +56,23 @@ app_license = "MIT"
 
 # add methods and filters to jinja environment
 # jinja = {
-#	"methods": "clientside.utils.jinja_methods",
-#	"filters": "clientside.utils.jinja_filters"
+# 	"methods": "clientside.utils.jinja_methods",
+# 	"filters": "clientside.utils.jinja_filters"
 # }
 
 # Installation
 # ------------
 
 # before_install = "clientside.install.before_install"
-# after_install = "clientside.install.after_install"
+after_install = "clientside.clientside.hooks.tps"
+import frappe
+
+
+def tps():
+    frappe.db.set_default("desktop:home_page", "workspace")
+    frappe.db.set_single_value("System Settings", "setup_complete", 1)
+    frappe.db.set_single_value("System Settings", "enable_onboarding", 1)
+
 
 # Uninstallation
 # ------------
@@ -83,11 +91,11 @@ app_license = "MIT"
 # Permissions evaluated in scripted ways
 
 # permission_query_conditions = {
-#	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
 # }
 #
 # has_permission = {
-#	"Event": "frappe.desk.doctype.event.event.has_permission",
+# 	"Event": "frappe.desk.doctype.event.event.has_permission",
 # }
 
 # DocType Class
@@ -95,7 +103,7 @@ app_license = "MIT"
 # Override standard doctype classes
 
 # override_doctype_class = {
-#	"ToDo": "custom_app.overrides.CustomToDo"
+# 	"ToDo": "custom_app.overrides.CustomToDo"
 # }
 
 # Document Events
@@ -103,32 +111,32 @@ app_license = "MIT"
 # Hook on document methods and events
 
 # doc_events = {
-#	"*": {
-#		"on_update": "method",
-#		"on_cancel": "method",
-#		"on_trash": "method"
-#	}
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+# 	}
 # }
 
 # Scheduled Tasks
 # ---------------
 
 # scheduler_events = {
-#	"all": [
-#		"clientside.tasks.all"
-#	],
-#	"daily": [
-#		"clientside.tasks.daily"
-#	],
-#	"hourly": [
-#		"clientside.tasks.hourly"
-#	],
-#	"weekly": [
-#		"clientside.tasks.weekly"
-#	],
-#	"monthly": [
-#		"clientside.tasks.monthly"
-#	],
+# 	"all": [
+# 		"clientside.tasks.all"
+# 	],
+# 	"daily": [
+# 		"clientside.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"clientside.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"clientside.tasks.weekly"
+# 	],
+# 	"monthly": [
+# 		"clientside.tasks.monthly"
+# 	],
 # }
 
 # Testing
@@ -140,14 +148,14 @@ app_license = "MIT"
 # ------------------------------
 #
 # override_whitelisted_methods = {
-#	"frappe.desk.doctype.event.event.get_events": "clientside.event.get_events"
+# 	"frappe.desk.doctype.event.event.get_events": "clientside.event.get_events"
 # }
 #
 # each overriding function accepts a `data` argument;
 # generated from the base implementation of the doctype dashboard,
 # along with any modifications made in other Frappe apps
 # override_doctype_dashboards = {
-#	"Task": "clientside.task.get_dashboard_data"
+# 	"Task": "clientside.task.get_dashboard_data"
 # }
 
 # exempt linked doctypes from being automatically cancelled
@@ -173,29 +181,29 @@ app_license = "MIT"
 # --------------------
 
 # user_data_fields = [
-#	{
-#		"doctype": "{doctype_1}",
-#		"filter_by": "{filter_by}",
-#		"redact_fields": ["{field_1}", "{field_2}"],
-#		"partial": 1,
-#	},
-#	{
-#		"doctype": "{doctype_2}",
-#		"filter_by": "{filter_by}",
-#		"partial": 1,
-#	},
-#	{
-#		"doctype": "{doctype_3}",
-#		"strict": False,
-#	},
-#	{
-#		"doctype": "{doctype_4}"
-#	}
+# 	{
+# 		"doctype": "{doctype_1}",
+# 		"filter_by": "{filter_by}",
+# 		"redact_fields": ["{field_1}", "{field_2}"],
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_2}",
+# 		"filter_by": "{filter_by}",
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_3}",
+# 		"strict": False,
+# 	},
+# 	{
+# 		"doctype": "{doctype_4}"
+# 	}
 # ]
 
 # Authentication and authorization
 # --------------------------------
 
 # auth_hooks = [
-#	"clientside.auth.validate"
+# 	"clientside.auth.validate"
 # ]
