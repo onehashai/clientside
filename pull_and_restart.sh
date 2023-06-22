f483ee3bfbfbc27bb959e698ed0355b582f29c0e
rohit.kumar-at-942428920331 <59276987+RohitKumarGit@users.noreply.github.com>
2023-06-22 11:22:46 +0530

    qwed

--- pull_and_restart.sh
+++ pull_and_restart.sh
@@ -10,6 +10,10 @@ cd /home/frappe/frappe-bench/apps/setup_app
 git add .
 git stash
 git pull
+cd /home/frappe/frappe-bench/apps/whitelabel
+git add .
+git stash
+git pull
 bench --site app.onehash.store migrate
 sudo supervisorctl restart all
 sudo bench setup production frappe
