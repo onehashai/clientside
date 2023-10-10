250922976cec816acb22808ab8ceff1ce1703413
Abhishek-Chougule <chouguleabhis@gmail.com>
2023-10-10 13:19:11 +0000

    Check Subscription

--- clientside/public/js/client.js
+++ clientside/public/js/client.js
@@ -1,71 +1,71 @@
-// clear cache
+// // clear cache
 
-async function checkIfUserHasActiveSubsciprion() {
-  let url = new URL(window.location.href);
-  const isOnboard = url.searchParams.get("onboard");
-  let method = "/api/method/clientside.stripe.hasActiveSubscription";
-  if (isOnboard) {
-    method += "?invalidate_cache=true";
-    // frappe.ui.toolbar.clear_cache();
-    // clear cache
-    // refresh page
-  }
+// async function checkIfUserHasActiveSubsciprion() {
+//   let url = new URL(window.location.href);
+//   const isOnboard = url.searchParams.get("onboard");
+//   let method = "/api/method/clientside.stripe.hasActiveSubscription";
+//   if (isOnboard) {
+//     method += "?invalidate_cache=true";
+//     // frappe.ui.toolbar.clear_cache();
+//     // clear cache
+//     // refresh page
+//   }
 
-  const hasActiveSubscription = await fetch(method, {
-    method: "GET",
-  }).then((r) => r.json());
-  if (hasActiveSubscription.message) {
-    return true;
-  }
-  return false;
-}
-async function checkIfUserHasRoleToManageOnehashPayments() {
-  const hasRoleToManageOnehashPayments = await fetch(
-    "/api/method/clientside.clientside.utils.hasRoleToManageOnehashPayments"
-  ).then((r) => r.json());
-  if (hasRoleToManageOnehashPayments.message) {
-    return true;
-  }
-  return false;
-}
-// plan page is accessible to only logged in users
-// on each login , we check if the user has an active subscription and update the cache
-// and return if this user has the role to manage onehash payments
-// if the user has the role to manage onehash payments, we show the plan page after login
-// if the user does not have the role to manage onehash payments, we redirect the user to the " Please contact site administrator to upgrade your plan" page
-function init() {
-  const url = new URL(window.location.href);
-  const isOnboard = url.searchParams.get("oneboard");
+//   const hasActiveSubscription = await fetch(method, {
+//     method: "GET",
+//   }).then((r) => r.json());
+//   if (hasActiveSubscription.message) {
+//     return true;
+//   }
+//   return false;
+// }
+// async function checkIfUserHasRoleToManageOnehashPayments() {
+//   const hasRoleToManageOnehashPayments = await fetch(
+//     "/api/method/clientside.clientside.utils.hasRoleToManageOnehashPayments"
+//   ).then((r) => r.json());
+//   if (hasRoleToManageOnehashPayments.message) {
+//     return true;
+//   }
+//   return false;
+// }
+// // plan page is accessible to only logged in users
+// // on each login , we check if the user has an active subscription and update the cache
+// // and return if this user has the role to manage onehash payments
+// // if the user has the role to manage onehash payments, we show the plan page after login
+// // if the user does not have the role to manage onehash payments, we redirect the user to the " Please contact site administrator to upgrade your plan" page
+// function init() {
+//   const url = new URL(window.location.href);
+//   const isOnboard = url.searchParams.get("oneboard");
 
-  // if (url.pathname.trim()==="/app" || url.pathname.trim()==="/app/home") {
-  checkIfUserHasActiveSubsciprion().then((hasActiveSubscription) => {
-    console.log("hasActiveSubscription", hasActiveSubscription);
-    if (!hasActiveSubscription) {
-      checkIfUserHasRoleToManageOnehashPayments().then(
-        (hasRoleToManageOnehashPayments) => {
-          console.log(
-            "hasRoleToManageOnehashPayments",
-            hasRoleToManageOnehashPayments
-          );
-          if (hasRoleToManageOnehashPayments) {
-            // show the plan page
-            console.log("show the plan page");
-            window.location.href = "/plans";
-          } else {
-            // redirect to "Please contact site administrator to upgrade your plan" page
-            console.log(
-              "redirect to Please contact site administrator to upgrade your plan page"
-            );
-            window.location.href = "/no-plan";
-          }
-        }
-      );
-    } else {
-      console.log("doing nothing");
-    }
-  });
-  // }
-}
-init();
+//   // if (url.pathname.trim()==="/app" || url.pathname.trim()==="/app/home") {
+//   checkIfUserHasActiveSubsciprion().then((hasActiveSubscription) => {
+//     console.log("hasActiveSubscription", hasActiveSubscription);
+//     if (!hasActiveSubscription) {
+//       checkIfUserHasRoleToManageOnehashPayments().then(
+//         (hasRoleToManageOnehashPayments) => {
+//           console.log(
+//             "hasRoleToManageOnehashPayments",
+//             hasRoleToManageOnehashPayments
+//           );
+//           if (hasRoleToManageOnehashPayments) {
+//             // show the plan page
+//             console.log("show the plan page");
+//             window.location.href = "/plans";
+//           } else {
+//             // redirect to "Please contact site administrator to upgrade your plan" page
+//             console.log(
+//               "redirect to Please contact site administrator to upgrade your plan page"
+//             );
+//             window.location.href = "/no-plan";
+//           }
+//         }
+//       );
+//     } else {
+//       console.log("doing nothing");
+//     }
+//   });
+//   // }
+// }
+// init();
 
-// disable right click
+// // disable right click
