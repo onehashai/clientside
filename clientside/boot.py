7374b0c78268a1735d7382c7aadefaf64bb05d7d
rishabh <rishabh@onehash.ai>
2025-06-11 17:32:55 +0530

    updated check subscription

--- clientside/boot.py
+++ clientside/boot.py
@@ -4,10 +4,21 @@ def extend_bootinfo(bootinfo):
     from frappe.utils import today, getdate, add_days
 
     subscription_ends_on = frappe.conf.subscription_ends_on
+    subscription_status = frappe.conf.subscription_status
 
-    if subscription_ends_on:
-        if getdate(today()) > add_days(getdate(subscription_ends_on), 5):
-            bootinfo.subscription_expired = True
+    bootinfo.subscription_expired = False
+
+    if subscription_status in ["unpaid", "canceled", "incomplete", "incomplete_expired"]:
+        bootinfo.subscription_expired = True
+
+
+    elif subscription_status in ["past_due", "active", "trialing"]:
+        if subscription_ends_on:
+            today_date = getdate(today())
+            expiry_date = getdate(subscription_ends_on)
+
+            if subscription_status != "trialing" and today_date > add_days(expiry_date, 5):
+                bootinfo.subscription_expired = True
 
     bootinfo.chat_widget_base_url = frappe.conf.chat_widget_base_url
     bootinfo.chat_widget_token = frappe.conf.chat_widget_token
