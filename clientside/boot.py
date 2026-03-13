319721d5eede06428f4cfb546787b215da9f4d3b
Sanskar Soni <sanskarsoni300@gmail.com>
2026-03-13 14:20:15 +0530

    feat(boot): consider invoice_due_date for site expiration
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- clientside/boot.py
+++ clientside/boot.py
@@ -1,23 +1,32 @@
 import frappe
 
+
 def extend_bootinfo(bootinfo):
     from frappe.utils import today, getdate, add_days
 
+    today_date = getdate(today())
+
     subscription_ends_on = frappe.conf.subscription_ends_on
     subscription_status = frappe.conf.subscription_status
+    invoice_due_date = frappe.conf.invoice_due_date
 
+    expiry_date = getdate(invoice_due_date or subscription_ends_on)
     bootinfo.subscription_expired = False
 
-    if subscription_status in ["unpaid", "canceled", "incomplete", "incomplete_expired", "paused"]:
+    if subscription_status in [
+        "unpaid",
+        "canceled",
+        "incomplete",
+        "incomplete_expired",
+        "paused",
+    ]:
         bootinfo.subscription_expired = True
 
-
     elif subscription_status in ["past_due", "trialing"]:
-        if subscription_ends_on:
-            today_date = getdate(today())
-            expiry_date = getdate(subscription_ends_on)
-
-            if subscription_status != "trialing" and today_date > add_days(expiry_date, 5):
+        if expiry_date:
+            if subscription_status != "trialing" and today_date > add_days(
+                expiry_date, 5
+            ):
                 bootinfo.subscription_expired = True
 
     bootinfo.chat_widget_base_url = frappe.conf.chat_widget_base_url
