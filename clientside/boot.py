e475da3674a2ac1f3e068910ee955701d2668c84
Sanskar Soni <sanskarsoni300@gmail.com>
2026-06-12 16:34:22 +0530

    feat: add internal site lifecycle exemption
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- clientside/boot.py
+++ clientside/boot.py
@@ -8,33 +8,40 @@ def get_config_date(*keys):
             return value
 
 
+def get_config_bool(key):
+    return frappe.conf.get(key) in (1, "1", True, "true", "True", "yes", "Yes")
+
+
 def extend_bootinfo(bootinfo):
     from frappe.utils import today, getdate, add_days
 
     today_date = getdate(today())
 
-    subscription_status = frappe.conf.subscription_status
-    site_expiry_date = get_config_date("site_expiry_date")
-    fallback_expiry_date = get_config_date("invoice_due_date", "subscription_ends_on")
-
-    expiry_date = getdate(site_expiry_date or fallback_expiry_date)
-    if not site_expiry_date and expiry_date:
-        expiry_date = add_days(expiry_date, 5)
     bootinfo.subscription_expired = False
+    if not get_config_bool("skip_subscription_expiry"):
+        subscription_status = frappe.conf.subscription_status
+        site_expiry_date = get_config_date("site_expiry_date")
+        fallback_expiry_date = get_config_date(
+            "invoice_due_date", "subscription_ends_on"
+        )
 
-    if subscription_status in [
-        "unpaid",
-        "canceled",
-        "incomplete",
-        "incomplete_expired",
-        "paused",
-    ]:
-        bootinfo.subscription_expired = True
+        expiry_date = getdate(site_expiry_date or fallback_expiry_date)
+        if not site_expiry_date and expiry_date:
+            expiry_date = add_days(expiry_date, 5)
 
-    elif subscription_status in ["past_due", "trialing"]:
-        if expiry_date:
-            if today_date > expiry_date:
-                bootinfo.subscription_expired = True
+        if subscription_status in [
+            "unpaid",
+            "canceled",
+            "incomplete",
+            "incomplete_expired",
+            "paused",
+        ]:
+            bootinfo.subscription_expired = True
+
+        elif subscription_status in ["past_due", "trialing"]:
+            if expiry_date:
+                if today_date > expiry_date:
+                    bootinfo.subscription_expired = True
 
     bootinfo.chat_widget_base_url = frappe.conf.chat_widget_base_url
     bootinfo.chat_widget_token = frappe.conf.chat_widget_token
